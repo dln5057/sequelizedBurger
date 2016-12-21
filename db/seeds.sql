a6da05df699f52e80123a1eb04b59c2473d72b2b
Donnie Nowels <djnowels11@gmail.com>
2016-12-21 16:31:30 -0500

    working on trying to get local host to show index page

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Onion Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Burger', false);
+
+use burgerDB;
+
+INSERT INTO burgers (burger_name, devoured, createdAt, updatedAt) 
+VALUES
+("Bob's burger", false, "2016-11-10 02:33:37","2016-11-10 02:33:37"),
+("Bill's burger", false, "2016-11-10 02:33:37","2016-11-10 02:33:37"),
+("Carl's burger", false, "2016-11-10 02:33:37","2016-11-10 02:33:37");
